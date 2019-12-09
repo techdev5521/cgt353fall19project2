3836aa2953a75bae633da610bb7e023576833f1b
Justin Campbell <justincampbell@Justins-MacBook-Pro-2.local>
2019-12-08 23:37:25 -0500

    Good-e-fucking-nuff

--- about.php
+++ about.php
@@ -12,39 +12,57 @@
 
 	<body>
 
-		<header>
-			<!-- Include Header Content -->
-			<?php include('includes/header.php'); ?>
+		<div class="container">
+			<header>
+				<!-- Include Header Content -->
+				<?php include('includes/header.php'); ?>
 
-			<!-- Include Navigation Content -->
-			<?php include('includes/navigation.php'); ?>
-		</header>
+				<!-- Include Navigation Content -->
+				<?php include('includes/navigation.php'); ?>
+			</header>
 
-		<main class="box background-white p-5 mb-5">
-			<div class="box" id="maincontainer">
-				
-				<div id="title">
-					<h1>What's this place all about??</h1>
+			<main class="box background-white p-5 mb-5">
+
+				<div class="row">
+					<div class="col">
+						<h1 class="title">Introduction</h1>
+					</div>
 				</div>
 
-				<div id="comic">
-					<p>Hey there! It looks like you've stumbled upon our little corner of the internet. If you like comics or want to try and make your own comic someday then this is the site for you! "A Web Comic about Making Web Comics" is an informational, educational site focused on leraning the fundamentals and genral wokrflow that most comic authors follow today. Here you will learn how to get your ideas down on paper, more efficient ways to brainstorm, and how to use comic assets. </p>
-					<h1>What's the point??</h1>
-					<p>Web comics are growing more and more popular by the day, and people are finding new ways to use them to relay stories and information to their readers. We want to show our support to the up and coming authors to try their hand at comic making by supplying them with the basic information they need to get started.</p>
-					<h1>What's in store??</h1>
-					<p>In the future we plan to have more modules available to readers as well as easily sharable information that users can give to their friends and family. The possibilities of workshops and instructional tutorial videos are also in the wokrs so stay tuned!</p>
+				<div class="row" id="comic">
+					<div class="col-12">
+						<p>Hey there! It looks like you've stumbled upon our little corner of the internet. If you like comics or want to try and make your own comic someday then this is the site for you! "A Web Comic about Making Web Comics" is an informational, educational site focused on leraning the fundamentals and genral wokrflow that most comic authors follow today. Here you will learn how to get your ideas down on paper, more efficient ways to brainstorm, and how to use comic assets. </p>
+					</div>
+
+					<div class="col-md-6">
+						<h2><strong>What's the point??</strong></h2>
+						<p>Web comics are growing more and more popular by the day, and people are finding new ways to use them to relay stories and information to their readers. We want to show our support to the up and coming authors to try their hand at comic making by supplying them with the basic information they need to get started.</p>
+					</div>
+
+					<div class="col-md-6">
+						<h2><strong>What's in store??</strong></h2>
+						<p>In the future we plan to have more modules available to readers as well as easily sharable information that users can give to their friends and family. The possibilities of workshops and instructional tutorial videos are also in the wokrs so stay tuned!</p>
+					</div>
+
+					<div class="col-12 text-center">
+						<h3><strong>Website Created By:</strong></h3>
+					</div>
+
+					<div class="col-4 text-center">
+						<p>Justin Campbell</p>
+					</div>
+					<div class="col-4 text-center">
+						<p>Ian Coffman</p>
+					</div>
+					<div class="col-4 text-center">
+						<p>Josh Hellman</p>
+					</div>
+					
+				</div>
 				</div>
 
-				<div>
-					<h1>Website Created By:</h1>
-					<br>
-					<h1>Ian Coffman</h1>
-					<h1>Josh Heller</h1>
-					<h1>Justin Campbell</h1>
-				</div>
-
-			</div>
-		</main>
+			</main>
+		</div>
 		
 	</body>
 </html>
--- contact.php
+++ contact.php
@@ -12,112 +12,65 @@
 
 	<body>
 
-		<header>
-			<!-- Include Header Content -->
-			<?php include('includes/header.php'); ?>
+		<div class="container">
+			<header>
+				<!-- Include Header Content -->
+				<?php include('includes/header.php'); ?>
 
-			<!-- Include Navigation Content -->
-			<?php include('includes/navigation.php'); ?>
-		</header>
+				<!-- Include Navigation Content -->
+				<?php include('includes/navigation.php'); ?>
+			</header>
 
-		<main class="box background-white p-5 mb-5">
-			<div class="box" id="maincontainer">
+			<main class="box background-white p-5 mb-5">
 
-				<div id="title">
-					<h1>Contact</h1>
+				<div class="row">
+					<div class="col">
+						<h1 class="title">Contact</h1>
+					</div>
 				</div>
 
-				<div id="comic">
-					<form action="https://formspree.io/mledpbvz" method="POST" onsubmit="return validation()" name="form" id="form">
-						<fieldset>
-							<legend>Please fill out the listed fields below</legend>
-						
-							<label for="firstName">First Name*</label>
-							<input type="text" name="firstName">
-						
-							<label for="lastName">Last Name*</label>
-						 	<input type="text" name="lastName">
+				<form action="https://formspree.io/mledpbvz" method="POST">
+					<div class="row" id="comic">
 
-							 <label for="_replyto">Email*</label>
-							<input type="email" name="_replyto" >
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="firstName">First Name*</label>
+								<input class="form-control" type="text" name="firstName">
+							</div>
+						</div>
 
-			 				<label for="message">What's on your mind?*</label>
-						 	<textarea name="message" id="message" ></textarea>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="lastName">Last Name*</label>
+						 		<input class="form-control" type="text" name="lastName">
+							</div>
+						</div>
 
-							<button type="submit">Submit that puppy!</button>
-						</fieldset>
-					</form>
-				</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="_replyto">Email*</label>
+								<input class="form-control" type="email" name="_replyto" >
+							</div>
+						</div>
 
-			</div>
-		</main>
+		 				<div class="col-12 d-flex justify-content-center">
+		 					<div class="form-group">
+			 					<label for="message">What's on your mind?*</label>
+						 		<textarea class="form-control" name="message" id="message" ></textarea>
+			 				</div>
+		 				</div>
+
+						<div class="col-12">
+							<input class="form-control btn btn-green" type="submit" value="Submit that Puppy!">
+						</div>
+					
+					</div>
+				</form>
+
+			</main>
+		</div>
 
 	</body>
-
-
-
-	<script>
-		function validateEmailAddress(email) {
-		var expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-
-			return expression.test(String(email).toLowerCase());
-		}
-
-		// Remove all error states -- used by permission from Casey
-	 	// Cass -- Casey wrote this comment ^^^ :)
-		resetErrors = () => {
-	 		const errorFields = document.getElementsByClassName("error");
-				if (errorFields.length > 0) {
-					[...errorFields].forEach(errorField => {
-						errorField.classList.remove("error");
-					});
-				}
-			}
-
- 		validation = () => {
-			event.preventDefault();
-
-			resetErrors(); 
-			let errorSpotted = false;
-
-			const firstName = document.forms["form"]["firstName"];
-			const firstNameValue = document.forms["form"]["firstName"].value.trim();
-			if (firstNameValue == "") {
-				firstName.parentNode.classList.add("error");
-				errorSpotted = true;
-			}
-	
-			const lastName = document.forms["form"]["lastName"];
-			const lastNameValue = document.forms["form"]["lastName"].value.trim();
-			if (lastNameValue == "") {
-				lastName.parentNode.classList.add("error");
-				errorSpotted = true;
-			}
-
-			const _replyto = document.forms["form"]["_replyto"];
-			const _replytoValue = document.forms["form"]["_replyto"].value.trim();
-			if (_replytoValue == "") {
-				_replyto.parentNode.classList.add("error");
-				errorSpotted = true;
-			}
-
-			if (!validateEmailAddress(_replytoValue)){
-				_replyto.parentNode.classList.add("errorEmail");
-				errorSpotted = true;
-			}
-
-			const message = document.forms["form"]["message"];
-			const messageValue = document.forms["form"]["message"].value.trim();
-			if (messageValue == "") {
-				message.parentNode.classList.add("error");
-				errorSpotted = true;
-			}
-
-			if (!errorSpotted){
-				document.getElementById("form").submit();
-			}
- 		}
-	</script>
 
 
 	
